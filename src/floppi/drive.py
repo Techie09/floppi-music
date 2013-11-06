9bb4efb70f081f583cf245c16eb9cfb01945670a
Dominik George <nik@naturalnet.de>
2013-11-06 12:15:02 +0100

    Finalized doxygen support.

--- src/floppi/drive.py
+++ src/floppi/drive.py
@@ -1,6 +1,25 @@
 ## @package floppi.drive
 #  Code to control a floppy drive for floppy music.
 
+# Copyright © 2013
+#       Dominik George <nik@naturalnet.de>
+#       Eike Tim Jesinghaus <eike@naturalnet.de>
+#
+# Provided that these terms and disclaimer and all copyright notices
+# are retained or reproduced in an accompanying document, permission
+# is granted to deal in this work without restriction, including un‐
+# limited rights to use, publicly perform, distribute, sell, modify,
+# merge, give away, or sublicence.
+#
+# This work is provided “AS IS” and WITHOUT WARRANTY of any kind, to
+# the utmost extent permitted by applicable law, neither express nor
+# implied; without malicious intent or gross negligence. In no event
+# may a licensor, author or contributor be held liable for indirect,
+# direct, other damage, loss, or other issues arising in any way out
+# of dealing in the work, even if advised of the possibility of such
+# damage or existence of a defect, except proven that it results out
+# of said person’s immediate fault when using the work as intended.
+
 from threading import Thread
 from time import sleep
 
@@ -9,8 +28,13 @@ from time import sleep
 #  This class controls one floppy drive in its own thread,
 #  so several drives can be used in parallel.
 class MusicalFloppy(Thread):
-    # Queue of tones to play
+    ## @var _playqueue
+    #  Queue of tones to play, containing tuples of (frequency, duration)
     _playqueue = []
+
+    ## @var _gpio
+    #  Holds reference to the GPIO object in use
+    _gpio = None
 
     ## Constructor.
     #
@@ -27,7 +51,12 @@ class MusicalFloppy(Thread):
         self._pin_direction = pins[0]
         self._pin_step = pins[1]
 
-    # Run the drive thread
+    ## Run the drive thread
+    #
+    #  Main loop function for the drive thread. It will set up the drive, then
+    #  start playing tones from the _playqueue list.
+    #
+    #  @param self the object pointer
     def run(self):
         # Explicitly set pins high
         self._gpio.high(self._pin_direction)
@@ -41,14 +70,23 @@ class MusicalFloppy(Thread):
                 tone = self._playqueue.pop(0)
                 self.tone(tone[0], tone[1])
 
-    # Move count tracks
+    ## Move the floppy head a certain number of tracks
+    #
+    #  @param self the object pointer
+    #  @param count the number of tracks to move
     def tracks(self, count):
         for i in range(count):
             self._gpio.toggle(self._pin_step)
             sleep(.002)
             self._gpio.toggle(self._pin_step)
 
-    # Move to home position, which is the middle track
+    ## Move to home position, which is the middle track
+    #
+    #  This method moves the head to the home position.
+    #  Currently, this is the middle track (40), but this may
+    #  change as we advance the floppy music quality.
+    #
+    #  @param self the object pointer
     def home(self):
         # Move 80 tracks, just to be sure
         self.tracks(80)
@@ -57,7 +95,16 @@ class MusicalFloppy(Thread):
         # Move 40 tracks back
         self.tracks(40)
 
-    # Shake the head with a certain interval, count times
+    ## Shake the head with a certain interval
+    #
+    #  Moves the head back and forth some times, pausing in between.
+    #  You can make sounds at certain frequencies by setting the interval
+    #  to 1/freq. Adjust the duration of the vibration by setting
+    #  the count to freq*duration.
+    #
+    #  @param self the object pointer
+    #  @param interval the pause between movements, in seconds
+    #  @param count how often to move the head
     def vibrate(self, interval, count):
         for i in range(count):
             self._gpio.toggle(self._pin_step)
@@ -65,10 +112,23 @@ class MusicalFloppy(Thread):
             self._gpio.toggle(self._pin_step)
             self._gpio.toggle(self._pin_direction)
 
-    # Play a certain frequency, for duration seconds
+    ## Play a certain frequency, for duration seconds
+    #
+    #  This method wraps the vibrate method with another form of arguments,
+    #  more convenient for playing notes.
+    #
+    #  @param self the object pointer
+    #  @param freq the frequency of the desired tone, in Hertz
+    #  @param duration duration of the tone, in seconds
     def tone(self, freq, duration):
         self.vibrate(1.0/freq, int(freq*duration))
 
-    # Add a toen to the play queue
+    ## Add a tone to the play queue
+    #
+    #  This method adds a tone to the _playqueue, so the main loop
+    #  will pick it up and play it.
+    #
+    #  @param self the object pointer
+    #  @param tone a tuple of (frequency, duration)
     def play(self, tone):
         self._playqueue.append(tone)
--- src/floppi/rpi.py
+++ src/floppi/rpi.py
@@ -1,16 +1,43 @@
+## @package floppi.rpi
+#  Wrapper for RPi.GPIO, reduced to what we need
+
+# Copyright © 2013
+#       Dominik George <nik@naturalnet.de>
+#       Eike Tim Jesinghaus <eike@naturalnet.de>
+#
+# Provided that these terms and disclaimer and all copyright notices
+# are retained or reproduced in an accompanying document, permission
+# is granted to deal in this work without restriction, including un‐
+# limited rights to use, publicly perform, distribute, sell, modify,
+# merge, give away, or sublicence.
+#
+# This work is provided “AS IS” and WITHOUT WARRANTY of any kind, to
+# the utmost extent permitted by applicable law, neither express nor
+# implied; without malicious intent or gross negligence. In no event
+# may a licensor, author or contributor be held liable for indirect,
+# direct, other damage, loss, or other issues arising in any way out
+# of dealing in the work, even if advised of the possibility of such
+# damage or existence of a defect, except proven that it results out
+# of said person’s immediate fault when using the work as intended.
 # Import original GPIO as _GPIO because we define our own GPIO later
+
 from RPi import GPIO as _GPIO
 
-# Exception to be thrown when trying to control a non-GPIO pin
+## Exception to be thrown when trying to control a non-GPIO pin
 class NoSuchGPIO(Exception):
     pass
 
-# GPIO class, reduced to what we need for Floppi-Music
+## GPIO class, reduced to what we need for Floppi-Music
 class GPIO:
-    # All pins that are GPIO pins
+    ## @var _gpios
+    #  All pins that are GPIO pins
     _gpios = (3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26)
 
-    # Constructor
+    ## Constructor
+    #
+    #  Initializes the whole GPIO header as output.
+    #
+    #  @param self the object pointer
     def __init__(self):
         # Set GPIO mode to native Broadcom
         _GPIO.setmode(_GPIO.BOARD)
@@ -19,20 +46,38 @@ class GPIO:
         for pin in self._gpios:
             _GPIO.setup(pin, _GPIO.OUT)
 
-    # Set a selected pin high, by native board pin number
+    ## Set a selected pin high, by native board pin number
+    #
+    #  @param self the object pointer
+    #  @param pin the native number of the pin to set
+    #
+    #  @throws NoSuchGPIO if the selected pin is not a GPIO pin
     def high(self, pin):
         if pin in self._gpios:
             _GPIO.output(pin, _GPIO.HIGH)
         else:
             raise NoSuchGPIO("Pin %d is not a GPIO pin." % pin)
 
-    # Set a selected pin low, by native board pin number
+    ## Set a selected pin low, by native board pin number
+    #
+    #  @param self the object pointer
+    #  @param pin the native number of the pin to set
+    #
+    #  @throws NoSuchGPIO if the selected pin is not a GPIO pin
     def low(self, pin):
         if pin in self._gpios:
             _GPIO.output(pin, _GPIO.LOW)
         else:
             raise NoSuchGPIO("Pin %d is not a GPIO pin." % pin)
 
+    ## Set a selected pin low if it was high, and high if it was low.
+    #
+    #  This method looks at the reported state of the pin.
+    #
+    #  @param self the object pointer
+    #  @param pin the native number of the pin to set
+    #
+    #  @throws NoSuchGPIO if the selected pin is not a GPIO pin
     def toggle(self, pin):
         if pin in self._gpios:
             _GPIO.output(pin, not _GPIO.input(pin))
